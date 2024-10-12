b3c6b38a150f80985b75276bec9b56cd82d8e239
ealpyspa <alpyspaeva.en@gmail.com>
2024-10-12 10:51:54 +0200

    Add config, spec, base tests

---
+++ src/main/java/com/example/teamcity/models/User.java
@@ -0,0 +1,16 @@
+package com.example.teamcity.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String username;
+    private String password;
+}
+
