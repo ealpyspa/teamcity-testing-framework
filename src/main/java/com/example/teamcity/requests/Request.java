ba2f87322afd18a6ba860b6848a76402044a61b1
ealpyspa <alpyspaeva.en@gmail.com>
2024-10-13 20:26:17 +0200

    Practice 7: added checked and unchecked requesters

--- src/main/java/com/example/teamcity/requests/Request.java
+++ src/main/java/com/example/teamcity/requests/Request.java
@@ -8,8 +8,8 @@ public class Request {
      * Request is a class that describes changing parameters of request such as
      * specification, endpoint (relative URL, model).
      */
-    private final RequestSpecification spec;
-    private final Endpoint endpoint;
+    protected final RequestSpecification spec;
+    protected final Endpoint endpoint;
 
     public Request(RequestSpecification spec, Endpoint endpoint) {
         this.spec = spec;
