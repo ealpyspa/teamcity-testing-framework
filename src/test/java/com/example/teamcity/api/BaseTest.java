d599bc70fe086a7508183713604c4b7a36dc63b8
ealpyspa <alpyspaeva.en@gmail.com>
2024-10-15 17:33:01 +0200

    Practice 13: implemented TestData class

--- src/test/java/com/example/teamcity/api/BaseTest.java
+++ src/test/java/com/example/teamcity/api/BaseTest.java
@@ -1,18 +1,23 @@
 package com.example.teamcity.api;
 
+import com.example.teamcity.models.TestData;
 import com.example.teamcity.requests.CheckedRequests;
 import com.example.teamcity.spec.Specifications;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.asserts.SoftAssert;
 
+import static com.example.teamcity.generators.TestDataGenerator.generate;
+
 public class BaseTest {
     protected SoftAssert softy;
     protected CheckedRequests superUserCheckRequests = new CheckedRequests(Specifications.superUserSpec());
+    protected TestData testData;
 
     @BeforeMethod(alwaysRun = true)
     public void beforeTest() {
         softy = new SoftAssert();
+        testData = generate();
     }
 
     @AfterMethod(alwaysRun = true)
